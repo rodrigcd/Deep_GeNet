46cbec1a12d312bc075135c935a263663403687c
Ignacio Reyes <ireyesj93@gmail.com>
2016-11-04 20:16:42 -0300

    model.py's main function has an example of a convnet training.

--- model.py
+++ model.py
@@ -2,17 +2,28 @@ import tensorflow as tf
 import numpy as np
 
 from .data import Database, CIFAR10
-from .layers import ConvolutionalLayer, FullyConnectedLayer
+from .layers import ConvolutionalLayer, MaxPoolingLayer, FullyConnectedLayer
 
 class NeuralNetwork(object):
     def __init__(self, database):
         self.database = database
-        # self.sess = tf.Session()
+        self.sess = tf.Session()
+        self.build_model()
+        self.optimizer = tf.train.AdamOptimizer(1e-4)
+        self.train_step = self.optimizer.minimize(self.loss)
+        self.correct_predictions = tf.equal(tf.argmax(self.model_output,1),
+                                            tf.argmax(self.target,1))
+        self.accuracy = tf.reduce_mean(
+            tf.cast(self.correct_predictions, tf.float32), name='accuracy')
+        self.sess.run(tf.initialize_all_variables())
+    
+    def build_model(self):
+        raise NotImplementedError("Must override NeuralNetwork.build_model()")
 
     def train_iterations(self, n_iterations):
         for iteration in range(n_iterations):
             data, labels = self.database.nextBatch()
-            self.train_step.eval(feed_dict={
+            _ = self.sess.run((self.train_step),feed_dict={
                 self.model_input: data,
                 self.target: labels
             })
@@ -20,7 +31,7 @@ class NeuralNetwork(object):
             feed_dict={
                 self.model_input: data,
                 self.target: labels
-                }
+                }, session=self.sess
             )
         return train_accuracy
             
@@ -40,7 +51,7 @@ class NeuralNetwork(object):
                 self.accuracy.eval(feed_dict={
                     self.model_input: data,
                     self.target: labels
-                    }
+                    }, session=self.sess
                 )
             )
         return np.array(accuracies).mean()
@@ -49,6 +60,27 @@ class ConvolutionalNetwork(NeuralNetwork):
     def __init__(self, database):
         super(ConvolutionalNetwork, self).__init__(database)
 
+    def build_model(self):
+        # Temporal implementation
+        self.model_input = tf.placeholder(tf.float32)
+        self.target = tf.placeholder(tf.float32)
+        self.conv_layer_1 = ConvolutionalLayer(self.model_input,
+                                               [5, 5, 3, 32],
+                                               'conv_layer_1')
+        self.fc_input = tf.reshape(self.conv_layer_1.output_tensor,
+                                   [-1, 32*32*32])
+        self.fc_layer = FullyConnectedLayer(self.fc_input,
+                                            [32*32*32, 10],
+                                            'fc_layer_1')
+        self.model_output = self.fc_layer.output_tensor
+        
+        self.loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(
+                self.model_output,
+                self.target,
+                name='loss')
+            )
+        
 class ResidualNetwork(NeuralNetwork):
     def __init__(self):
         super(ResidualNetwork, self).__init__(database)
@@ -56,5 +88,10 @@ class ResidualNetwork(NeuralNetwork):
 
 if __name__=='__main__':
     database = CIFAR10(batch_size=100)
-    data, labels = database.getTestSet()
+    data, labels = database.getTestSet(asBatches=False)
     print len(data), len(labels)
+    convnet = ConvolutionalNetwork(database)
+    print convnet.evaluate()
+    convnet.train_iterations(100)
+    print convnet.evaluate()
+    
