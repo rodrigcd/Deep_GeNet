dbd0c84cd2f2423719eeb38e70736d6df0df9047
Ignacio Reyes <ireyesj93@gmail.com>
2017-03-01 16:11:09 -0300

    Readme file was modified, some minor changes.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Deep-GeNet
+
+This is the code for the project Deep-GeNet, which trains a convolutional neural network with a novel strategy that mixes genetic algorithms and gradient descent.
+
+To run the code execute the following command from the console on the parent directory, where "Deep-GeNet" is the name of the folder that contains the code.
+>> python2 -m Deep-GeNet.demo
+
+This command will execute the Deep-GeNet algorithm and it will save a pickle file with the fitness of the population and the proportion of sons for each generation.
+
+A traditional gradient descent training can be done using the following command from the parent directory:
+>> python2 -m Deep-GeNet.simple_training
--- simple_training.py
+++ simple_training.py
@@ -11,53 +11,6 @@ from .model import NeuralNetwork, ConvolutionalNetwork
 '''[[(3, 3, 3, 32), (32,)], [(3, 3, 32, 32), (32,)], [(3, 3, 32, 64), (64,)], [(3, 3, 64, 64), (64,)], [(3, 3, 64, 128), (128,)], [(3, 3, 128, 128), (128,)], [(3, 3, 128, 128), (128,)]]
 '''
 
-#database = CIFAR10(batch_size = 100)
-#data, labels = database.getTestSet(asBatches=False)
-#print len(data), len(labels)
-#convnet = ConvolutionalNetwork(database)
-#params = convnet.get_params()
-#gen = Genome(params)
-#print gen
-
-#n_iter = 10
-#print "creating"
-#indiv1 = Individual()
-#print indiv1.fitness
-#indiv2 = Individual()
-#print "growing"
-#indiv1.grow(n_iter)
-#print indiv1.fitness
-#print "crossover"
-#ind3, ind4 = indiv1.crossover(indiv2, n_iter)
-#print ind3.fitness
-#print "growing child"
-#ind3.grow(n_iter)
-#print ind3.fitness
-#print "mutating"
-#ind3.mutate(1)
-#print ind3.fitness
-#print "done"
-
-
-# TEST FOR INDIVIDUAL RECOVERY -> IS WORKING :D
-#print "\n"*10,"Recovering individual test"
-#indiv = Individual()
-#indiv.grow_all_params(1000, first_iter=True)
-#print "Fitness after 1000 iters %.2f"%(indiv.fitness)
-#indiv.repair_and_grow(1000, repair_iters=400)
-#print "\n"*10
-
-
-#fitness = list()
-#n_gen = 100
-#population = Population(n_indiv=20)
-#for i in range(n_gen):
-#    fitness.append(population.iter())
-#
-#with open('workfile.pkl', 'w') as f:
-#    pickle.dump({'fitness': fitness}, f, pickle.HIGHEST_PROTOCOL)
-
-
 database = CIFAR10(batch_size=100, augment_data=True)
 convnet = ConvolutionalNetwork(database)
 for i in range(1500):
